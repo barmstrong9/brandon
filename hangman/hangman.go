9f7304df14dbfbe694ca9c0bdebf3ca9fba8ff61
Brandon Armstrong <brandon.armstrong@thingco.com>
2018-09-18 16:03:14 +0100

    hangman.go

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -1,18 +1,49 @@
 package main
+
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"time"
+	"log"
 )
 
 func main() {
-	wordList := []string{"cactus","mobile","window","laptop","monitor"}
+	fmt.Println("Welcome To Hangman")
+	wordList := []string{"cactus", "mobile", "window", "laptop", "monitor"}
 	lengthOfWords := len(wordList)
 	randomNumber := generateRandomNumber(lengthOfWords)
-	fmt.Println(randomNumber)	
+	fmt.Println(randomNumber)
+	wordToGuess := wordList[randomNumber]
+	log.Println(wordToGuess)
+	input := ""
+	for {
+		fmt.Printf("Guess a letter:")
+
+		fmt.Scanln(&input)
+		isValid := isValidLetter(input)
+			if isValid{
+				break
+			}else {
+				fmt.Println("Not a valid input")
+			}
+		
+	}
+	fmt.Println(input)
 }
-func generateRandomNumber(lenWords int)int{
+func generateRandomNumber(lenWords int) int {
 	seed := rand.NewSource(time.Now().UnixNano())
 	rand := rand.New(seed)
-	return rand.Intn(4)
+	return rand.Intn(5)
+}
+func isValidLetter(input string) bool {
+	if len(input) == 1 {
+		isLetter, _ := regexp.MatchString("^[a-zA-Z]", input)
+		if isLetter {
+			return true
+		}
+
+	}
+	return false
+
 }
--- hangman/hangman_test.go
+++ hangman/hangman_test.go
@@ -12,3 +12,19 @@ func isLessThan(num1, num2 int)bool{
 	return num1 < num2 
 		
 }
+func TestValidateLetter(t *testing.T){
+	var tests = []struct{
+		input string
+		expected bool
+	}{
+		{"a",true},
+		{"aa", false},
+		{"!", false},
+		{"9", false},
+	}
+	for _, test := range tests{
+		result := isValidLetter(test.input)
+	assert.Equal(t, test.expected, result)
+	}
+	
+}
