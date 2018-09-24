2fd3615f79c989e8279d1d00f372c7d182565f98
barmstrong9 <43335596+barmstrong9@users.noreply.github.com>
2018-09-24 12:15:30 +0100

    Add files via upload

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv in the format 'question, answer'")
+	timeLimit := flag.Int("limit", 30, "the time left for the quiz in seconds")
 	flag.Parse()
 	_ = csvFilename
 
@@ -23,13 +25,25 @@ func main() {
 		exit("Failed To Parse The Given CSV File")
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou got %d out of %d correct\n", correct, len(problems))
+			return
+		case answer := <- answerCh:
+			if answer == p.a {
+				correct++
+			}
+
 		}
 	}
 	fmt.Printf("You got %d out of %d correct", correct, len(problems))
